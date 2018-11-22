3f597b4b936ece57fa53da19d16e23a63df56a3d
fighting123 <1102194607@qq.com>
2018-11-22 14:41:05 +0800

    'path'

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -170,45 +170,6 @@ module.exports = {
           // tags. If you use code splitting, however, any async bundles will still
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
-          // {
-          //   test: /\.less$/,
-          //   use: [
-          //     require.resolve('style-loader'),
-          //     {
-          //       loader: require.resolve('css-loader'),
-          //       options: {
-          //         importLoaders: 1,
-          //         modules: false,
-          //         modifyVars: {
-          //           '@primary-color': '#f9c700'  // 项目的主色调
-          //         }
-          //       },
-          //     },
-          //     {
-          //       loader: require.resolve('postcss-loader'),
-          //       options: {
-          //         // Necessary for external CSS imports to work
-          //         // https://github.com/facebookincubator/create-react-app/issues/2677
-          //         ident: 'postcss',
-          //         plugins: () => [
-          //           require('postcss-flexbugs-fixes'),
-          //           autoprefixer({
-          //             browsers: [
-          //               '>1%',
-          //               'last 4 versions',
-          //               'Firefox ESR',
-          //               'not ie < 9', // React doesn't support IE8 anyway
-          //             ],
-          //             flexbox: 'no-2009',
-          //           }),
-          //         ],
-          //       },
-          //     },
-          //     {
-          //       loader: require.resolve('less-loader')
-          //     }
-          //   ],
-          // },
           {
             test: /\.(css|less)$/,
             loader: ExtractTextPlugin.extract(
@@ -226,6 +187,7 @@ module.exports = {
                       options: {
                         importLoaders: 1,
                         minimize: true,
+                        modules:true, //新添加的
                         sourceMap: shouldUseSourceMap,
                       },
                     },
@@ -253,13 +215,12 @@ module.exports = {
                       loader: require.resolve('less-loader'),
                       options: {
                         importLoaders: 1,
-                        modules: true,
-                        sourceMap: shouldUseSourceMap,
+                        modules: false,
                         modifyVars: {
                           '@primary-color': '#f9c700'  // 项目的主色调
                         }
                       },
-                    }
+                    },
                   ],
                 },
                 extractTextPluginOptions
